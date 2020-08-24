0f33e3631d56c5cbb5ebbfe5df579cac7fbe94bf
Hack-Light <onohsomtochukwubasil@gmail.com>
2020-08-24 17:12:43 +0100

    Otp verification now works

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 
 let registerRoute = require("./routes/register");
 let loginRoute = require("./routes/login");
+let otpRoute = require("./routes/otp");
 
 mongoose
   .connect(process.env.DB_URI, {
@@ -32,6 +33,7 @@ app.use(express.static(path.join(__dirname, "assets")));
 
 app.use(registerRoute);
 app.use(loginRoute);
+app.use(otpRoute);
 // app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
