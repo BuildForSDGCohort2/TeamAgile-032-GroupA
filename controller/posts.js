c3e451646a4aff40e1126f66cd8a039a2398a4bb
Hack-Light <onohsomtochukwubasil@gmail.com>
2020-08-27 12:34:17 +0100

    post upload is now complete

--- controller/posts.js
+++ controller/posts.js
@@ -1,15 +1,17 @@
 const postSchema = require("../models/post");
 const userSchema = require("../models/user");
+const DataUri = require("datauri/parser");
+const path = require("path");
+const cloudinary = require("cloudinary");
 
 exports.upload = async (req, res, next) => {
-  console.log(req.user);
+  let postTitle = req.body.title;
+  let postText = req.body.text || null;
+
+  let phone = req.user.phone_number;
+  console.log(req.files);
+
   try {
-    let postTitle = req.body.title;
-    let postText = req.body.text || null;
-    let mediaFiles = req.file || null;
-
-    let phone = req.user.phone_number;
-
     let user = await userSchema.findOne({ phone });
     if (!user) {
       return res.status(404).json({
@@ -29,14 +31,34 @@ exports.upload = async (req, res, next) => {
       author: author
     };
 
-    post = await postSchema.create(post);
+    if (req.files) {
+      post.media = [];
+      let dtauri = new DataUri();
+      for (const file of req.files) {
+        let dataUri = dtauri.format(
+          path.extname(file.originalname),
+          file.buffer
+        );
+
+        let final_file = dataUri.content;
+
+        let image = await cloudinary.v2.uploader.upload_large(final_file);
+
+        post.media.push({
+          url: image.secure_url,
+          public_id: image.public_id
+        });
+      }
+    }
+
+    let post_result = await postSchema.create(post);
 
     return res.status(200).json({
       success: true,
       message: "Post created successfully.",
       data: {
         statusCode: 200,
-        post
+        post: post_result
       }
     });
   } catch (err) {
