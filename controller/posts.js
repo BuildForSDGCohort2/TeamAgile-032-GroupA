d799665fd0d72801bd5fe5058145485d9c467ccd
Hack-Light <onohsomtochukwubasil@gmail.com>
2020-09-07 19:27:11 +0100

    User and admins can now delete posts

--- controller/posts.js
+++ controller/posts.js
@@ -179,3 +179,111 @@ exports.getAllAdmin = async (req, res, next) => {
     });
   }
 };
+
+exports.deletePostByAuthor = async (req, res, next) => {
+  let postID = req.params.id;
+  try {
+    let { phone_number, role } = req.user;
+    let user = await userSchema.findOne({
+      phone: phone_number
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+        error: {
+          statusCode: 404,
+          description: "User not found"
+        }
+      });
+    }
+
+    let post = await postSchema
+      .findById(postID)
+      .populate("author", "phone")
+      .exec();
+
+    if (post.author.phone != phone_number) {
+      return res.status(401).json({
+        success: false,
+        message: "You are not authorised to access this resource",
+        error: {
+          statusCode: 401,
+          description: "You are not authorised to access this resource"
+        }
+      });
+    }
+
+    let result = await postSchema.findByIdAndDelete(postID);
+    return res.status(200).json({
+      success: true,
+      message: "Post successfully deleted",
+      data: {
+        statusCode: 200,
+        data: result
+      }
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+      error: {
+        statusCode: err.statusCode,
+        description: err.message
+      }
+    });
+  }
+};
+
+exports.deletePostByAdmin = async (req, res, next) => {
+  let postID = req.params.id;
+  try {
+    let { phone_number, role } = req.user;
+
+    let user = await userSchema.findOne({
+      phone: phone_number
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+        error: {
+          statusCode: 404,
+          description: "User not found"
+        }
+      });
+    }
+
+    if (user.role != "super_admin") {
+      return res.status(401).json({
+        success: false,
+        message: "You are not authorised to access this resource",
+        error: {
+          statusCode: 401,
+          description: "You are not authorised to access this resource"
+        }
+      });
+    }
+
+    let result = await postSchema.findByIdAndDelete(postID);
+    return res.status(200).json({
+      success: true,
+      message: "Post successfully deleted",
+      data: {
+        statusCode: 200,
+        data: result
+      }
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+      error: {
+        statusCode: err.statusCode,
+        description: err.message
+      }
+    });
+  }
+};
