10d28a40ca14efea4b23a64ba8730d7bf911f906
Hack-Light <onohsomtochukwubasil@gmail.com>
2020-09-07 15:56:53 +0100

    Admin can now delete any user

--- controller/user.js
+++ controller/user.js
@@ -49,3 +49,54 @@ exports.getAllUsers = async (req, res, next) => {
     });
   }
 };
+
+exports.deleteUser = async (req, res, next) => {
+  try {
+    let userID = req.params.id;
+    let { phone_number, role } = req.user;
+    let user = await UserSchema.findOne({
+      phone: phone_number
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+        error: {
+          statusCode: 404,
+          description: "User not found"
+        }
+      });
+    }
+
+    if (role !== "super_admin") {
+      return res.status(401).json({
+        success: false,
+        message: "You are not authorised to access this resource",
+        error: {
+          statusCode: 401,
+          description: "You are not authorised to access this resource"
+        }
+      });
+    }
+
+    let result = await UserSchema.findByIdAndDelete(userID);
+    return res.status(200).json({
+      success: true,
+      message: "User successfully deleted",
+      data: {
+        statusCode: 200,
+        data: result
+      }
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: err.message,
+      error: {
+        statusCode: err.statusCode,
+        description: err.message
+      }
+    });
+  }
+};
